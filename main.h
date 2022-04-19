25cb1c7f5f33e56c3658f4d2b687612a5dcc362c
caren-achieng <caren.achieng@strathmore.edu>
2022-04-19 21:34:59 +0300

    partner work

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
+int _putchar(char c);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int _printf(const char *format, ...);
+
+#endif
